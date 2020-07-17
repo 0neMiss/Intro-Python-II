1cd2f5810f6805c5fb15be1077149d37b89382b9
Jordan <jordanshehane@gmail.com>
2020-07-17 12:40:20 -0600

    finished

--- src/adv.py
+++ src/adv.py
@@ -1,22 +1,22 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("narrow", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("treasure", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -35,12 +35,46 @@ room['treasure'].s_to = room['narrow']
 
 #
 # Main
-#
-
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(room['outside'])
 # Write a loop that:
 #
+print(player.room)
+running = True
+while(running == True):
+    print(player.room)
+    direction = input('enter a cardinal direction')
+    if direction == 'east':
+        try:
+            player.room = room[player.room.name].e_to
+            print(player.room)
+        except AttributeError:
+            print('wrong way idiot')
+            pass
+    elif direction == 'west':
+        try:
+            player.room = room[player.room.name].w_to
+            print(player.room)
+        except AttributeError:
+            print('wrong way idiot')
+            pass
+    elif direction == 'north':
+        try:
+            player.room = room[player.room.name].n_to
+            print(player.room)
+        except AttributeError:
+            print('wrong way idiot')
+            pass
+    elif direction == 'south':
+        try:
+            player.room = room[player.room.name].s_to
+            print(player.room)
+        except AttributeError:
+            print('wrong way idiot')
+            pass
+    elif direction == 'q':
+        break
+
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,8 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room(object):
+    def __init__(self, name, discription):
+        self.name = name
+        self.discription = discription
+    def __str__(self):
+        return self.name + self.discription
